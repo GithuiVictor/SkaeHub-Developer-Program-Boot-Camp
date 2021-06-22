893204109d266a170bf49393658f6de6f00bb66b
GithuiVctor <victorgithuim@gmail.com>
2021-06-22 15:45:39 +0300

    challenge 3

--- DAY 1/challenge3.py
+++ DAY 1/challenge3.py
@@ -1,22 +1,6 @@
 import random
 import string
 
-# length =int(input("\n Please enter what length you'd like your:"))
-
-# #Define data
-# lower = string.ascii_lowercase
-# upper =  string.ascii_uppercase
-# num = string.digits
-# symbols = string.punctuation
-
-# #combine all the data
-# all = lower + upper + num + symbols
-
-# #Generate passwords
-# password = "".join(random.sample(all, length))
-
-# print("Your new password is: ", password)
-
 class generate_password:
     def __init__ (self, length):
         self.length = length
@@ -35,4 +19,4 @@ class generate_password:
         return "Your new generated password is: {}".format(password)
 
 length = generate_password(int(input("\n Please enter what length you'd like your:")))
-print(generate_password.password( length))
+print(generate_password.password(length))
