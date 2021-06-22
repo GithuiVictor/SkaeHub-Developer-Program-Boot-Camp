ce37aa2d046de5464bb0841a11608cc87fe052a3
GithuiVctor <victorgithuim@gmail.com>
2021-06-22 13:07:41 +0300

    Challange 3

--- DAY 1/challenge3.py
+++ DAY 1/challenge3.py
@@ -1,18 +1,38 @@
 import random
 import string
 
-length =int(input("\n Please enter what length you'd like your:"))
+# length =int(input("\n Please enter what length you'd like your:"))
 
-#Define data
-lower = string.ascii_lowercase
-upper =  string.ascii_uppercase
-num = string.digits
-symbols = string.punctuation
+# #Define data
+# lower = string.ascii_lowercase
+# upper =  string.ascii_uppercase
+# num = string.digits
+# symbols = string.punctuation
 
-#combine all the data
-all = lower + upper + num + symbols
+# #combine all the data
+# all = lower + upper + num + symbols
 
-#Generate passwords
-password = "".join(random.sample(all, length))
+# #Generate passwords
+# password = "".join(random.sample(all, length))
 
-print("Your new password is: ", password)
+# print("Your new password is: ", password)
+
+class generate_password:
+    def __init__ (self, length):
+        self.length = length
+    
+    def password(self):
+        lower = string.ascii_lowercase
+        upper =  string.ascii_uppercase
+        num = string.digits
+        symbols = string.punctuation
+
+        #combine all data
+        all = lower + upper + num + symbols
+
+        password = "".join(random.sample(all, self.length))
+
+        return "Your new generated password is: {}".format(password)
+
+length = generate_password(int(input("\n Please enter what length you'd like your:")))
+print(generate_password.password( length))
