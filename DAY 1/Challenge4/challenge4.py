2abcdeaeabee3447777791ee72b12a18301f2993
GithuiVctor <victorgithuim@gmail.com>
2021-06-23 12:51:30 +0300

    Challenge Three

--- DAY 1/Challenge4/challenge4.py
+++ DAY 1/Challenge4/challenge4.py
@@ -1,7 +1,18 @@
+# import csv
+
+# data = csv.DictReader(open("departments.csv"))
+# print("CSV file as a dictionary: \n")
+
+# for row in data:
+#     print(row)
+
 import csv
-
-data = csv.DictReader(open("departments.csv"))
-print("CSV file as a dictionary: \n")
-
-for row in data:
-    print(row)
+#opening the file and reading it
+#r opens the file
+with open("job.csv", 'r') as file:
+    #creating an object
+    csv_file = csv.DictReader(file)
+    #for loop for iterating over the csv_file object
+    for row in csv_file:
+        #creating dictionaries
+        print(dict(row))
