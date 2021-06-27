0a7909e826f0f591d0c6705de6620c2733154e52
GithuiVictor <victorgithuim@gmail.com>
2021-06-27 20:57:39 +0300

    Update: SYntax

--- DAY 4/Challenge_3/challenge3.py
+++ DAY 4/Challenge_3/challenge3.py
@@ -4,18 +4,18 @@ from geopy.geocoders import Nominatim
 # initialize Nominatim API 
 geolocator = Nominatim(user_agent="geoapiExercises")
 
-def city_state_country(location_corrdinate):
-    # to capture Latitude & Longitude
-    location = geolocator.reverse(location_corrdinate, exactly_one=True)
-    address = location.raw['address']
+location_corrdinate = "1.2933, 38.8400"
 
-    # traverse the data
-    city = address.get('city', '')
-    state = address.get('state', '')
-    country = address.get('country', '')
-    code = address.get('country_code')
-    zipcode = address.get('postcode')
-    return city, state, country,code,zipcode
+# to capture Latitude & Longitude
+location = geolocator.reverse(location_corrdinate, exactly_one=True)
+address = location.raw['address']
+
+# traverse the data
+city = address.get('city', '')
+state = address.get('state', '')
+country = address.get('country', '')
+code = address.get('country_code')
+zipcode = address.get('postcode')
 
 # Latitude & Longitude input
-print(city_state_country("1.2933, 38.8400"))
+print("{} {} {} {} {}".format(city, state, country, code, zipcode))
