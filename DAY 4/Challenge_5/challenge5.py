02281c0636324c31a1bab573363a5920ca0e4c88
GithuiVictor <victorgithuim@gmail.com>
2021-06-28 14:20:01 +0300

    challenge

--- DAY 4/Challenge_5/challenge5.py
+++ DAY 4/Challenge_5/challenge5.py
@@ -1,10 +1,7 @@
 import itertools as it
-
 def infinite_data(iter):
-    return it.cycle(iter) 
-  
+    return it.cycle(iter)   
 # Dictionary
 result = infinite_data({1: 'Skae', 2: 'Hub', 3: 'Developer'})
-
 for k in result:
     print(k)
